e7f099d7319fd3e8e6f95a6beb995ee3949935c0
shihuaxie <sylvia501628@gmail.com>
2025-11-27 16:41:24 +1100

    Add tsk 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function concat
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Return the concatenation of str1 and str2
+    """
+    return str1 + str2
