346223efc041d57c4560b5767d53e4ea174b2272
shihuaxie <sylvia501628@gmail.com>
2025-12-06 15:49:08 +1100

    Add tsk9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Type-annotated function element_length
+"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Return a list of tuples (element, length of element)
+    """
+    return [(i, len(i)) for i in lst]
