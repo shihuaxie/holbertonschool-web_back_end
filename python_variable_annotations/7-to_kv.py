52d61da3bf336be41bb508c1904975b23323c62f
shihuaxie <sylvia501628@gmail.com>
2025-12-06 15:40:21 +1100

    add task 7

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function to_kv
+"""
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Return a tuple with the string k and the square of v as a float
+    """
+    return (k, float(v ** 2))
