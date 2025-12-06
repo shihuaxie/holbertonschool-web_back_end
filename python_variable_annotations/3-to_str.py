999f145b2f28a7e0693deef3f3d45617ed370f4c
shihuaxie <sylvia501628@gmail.com>
2025-12-06 15:11:46 +1100

    Add task 3

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Type-annotated function to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Return the string representation of the float n
+    """
+    return str(n)
