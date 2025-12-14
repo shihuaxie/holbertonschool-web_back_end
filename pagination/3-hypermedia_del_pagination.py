03752fffa7524f58f269b8d540a45050be6fcb47
shihuaxie <sylvia501628@gmail.com>
2025-12-14 18:48:40 +1100

    add task 3

---
+++ pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by original position"""
+        if self.__indexed_dataset is None:
+            self.__indexed_dataset = {
+                i: row for i, row in enumerate(self.dataset())
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+        """
+        Deletion-resilient pagination
+        """
+        assert isinstance(index, int) and index >= 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data = []
+        indexed = self.indexed_dataset()
+        current_index = index
+
+        while len(data) < page_size and current_index < len(indexed):
+            if current_index in indexed:
+                data.append(indexed[current_index])
+            current_index += 1
+
+        return {
+            "index": index,
+            "next_index": current_index,
+            "page_size": len(data),
+            "data": data
+        }
