c826e2cd790530f3c82d5ab2d0ee14d5ee1d9428
shihuaxie <sylvia501628@gmail.com>
2026-01-02 15:54:02 +1100

    Add task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+
+  return Array.from(set)
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length))
+    .join('-');
+}
