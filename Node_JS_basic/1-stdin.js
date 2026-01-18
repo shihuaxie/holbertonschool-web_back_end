7adf27fe33ea1c838bf9629d0a233990549075c3
Sylvia <102712823+shihuaxie@users.noreply.github.com>
2026-01-18 15:57:32 +1100

    Update 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf-8');
 
-process.stdin.on('data', data => {
+process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
 });
 
