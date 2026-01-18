5d7199e14419f648748a020f95cef3bb5b0c6c6b
shihuaxie <sylvia501628@gmail.com>
2026-01-18 15:29:21 +1100

    fix: rejected value must be a string

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,9 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then(
     (results) =>
-      results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
+      results.map((res) => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : res.reason.toString(),
       }))
   );
 }
